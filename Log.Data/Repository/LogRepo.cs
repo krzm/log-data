416172d0240e859b01409890ed6b8babb0841518
krzm <kmazanek@gmail.com>
2022-02-19 20:09:40 +0100

    Update LogRepo.cs
    Add Task.Category to data of logs

--- Log.Data/Repository/LogRepo.cs
+++ Log.Data/Repository/LogRepo.cs
@@ -15,15 +15,18 @@ public class LogRepo
 
     public IEnumerable<LogModel> GetFromTodayOrDateOrBefore(DateTime? dateParam)
     {
-        var date = dateParam.HasValue ? dateParam.Value.Date : DateTime.Now.Date;
+        var date = dateParam.HasValue ? 
+            dateParam.Value.Date : DateTime.Now.Date;
         return GetByDate(date);
     }
 
     public IEnumerable<LogModel> GetByDate(DateTime dateTime)
     {
         return Get(
-            filter: (l) => l.Start.HasValue ? l.Start.Value.Date.Equals(dateTime) : true
-            , orderBy: (l) => l.OrderBy((ls) => ls.Start).ThenBy((le) => le.End)
-            , includeProperties: "Task,Place");
+            filter: (l) => l.Start.HasValue ? 
+                l.Start.Value.Date.Equals(dateTime) : true
+            , orderBy: (l) => l.OrderBy((ls) => ls.Start)
+                .ThenBy((le) => le.End)
+            , includeProperties: "Task,Place,Task.Category");
     }
 }
